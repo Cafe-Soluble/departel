14726821a93bba62f37f278732a59c9af11605c1
Cafe Soluble <jeanchri.garcia@gmail.com>
2024-03-06 23:43:50 +0100

    dynamic year footer

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from config import Config
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,5 +10,8 @@ app.config.from_object(Config)
 # Initialisez CSRFProtect
 csrf = CSRFProtect(app)
 
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
 
 from app import routes
